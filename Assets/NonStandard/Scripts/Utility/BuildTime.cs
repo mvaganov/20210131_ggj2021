06ced40a03d418a502cb1fdfda4738d4ff7be9ff
Michael Vaganov <mvaganov@hotmail.com>
2021-04-03 01:00:46 -0700

    timestamp fix

--- Assets/NonStandard/Scripts/Utility/BuildTime.cs
+++ Assets/NonStandard/Scripts/Utility/BuildTime.cs
@@ -10,13 +10,12 @@ using UnityEngine;
 class MyCustomSceneProcessor : IProcessSceneWithReport {
     public int callbackOrder { get { return 0; } }
     public void OnProcessScene(UnityEngine.SceneManagement.Scene scene, BuildReport report) {
-        Debug.Log("MyCustomSceneProcessor.OnProcessScene " + scene.name);
         string path = Show.GetStackFullPath(1, 0)[0];
         char P = Path.DirectorySeparatorChar;
-        path = path.Substring(0, path.LastIndexOf(P) + 1);
-        //string filename = path + "V.cs", filedata = "namespace NonStandard{public static class V{public static string v=\"" +DateTime.Now.ToString("yyyyMMdd.HHmmss") + "\";}}";
-        //File.WriteAllText(filename, filedata);
-        File.WriteAllText(path + "Resources" + P + "app_build_time.txt", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+        path = path.Substring(0, path.LastIndexOf(P)); // NonStandard/Scripts/Utility
+        path = path.Substring(0, path.LastIndexOf(P)); // NonStandard/Scripts
+        path = path.Substring(0, path.LastIndexOf(P)); // NonStandard
+        File.WriteAllText(path + P + "Resources" + P + "app_build_time.txt", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
         AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
     }
 }
@@ -24,13 +23,6 @@ class MyCustomBuildProcessor : IPreprocessBuildWithReport {
     public int callbackOrder { get { return 0; } }
     public void OnPreprocessBuild(BuildReport report) {
         Debug.Log("MyCustomBuildProcessor.OnPreprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
-        string path = Show.GetStackFullPath(1, 0)[0];
-        char P = Path.DirectorySeparatorChar;
-        path = path.Substring(0, path.LastIndexOf(P) + 1);
-        //string filename = path + "V.cs", filedata = "namespace NonStandard{public static class V{public static string v=\"" +DateTime.Now.ToString("yyyyMMdd.HHmmss") + "\";}}";
-        //File.WriteAllText(filename, filedata);
-        File.WriteAllText(path+"Assets"+P+"Resources"+P+"app_build_time.txt", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")); 
-        AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
     }
 }
 #endif
